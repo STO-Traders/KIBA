fd4b2536b815edd8c8343de8dd6b7f7c4e875f19
Johan Jo <tradehaus@Mac.localdomain>
2026-06-16 11:30:36 -0500

    Add uncensored profane spinner words

--- src/repl/core.py
+++ src/repl/core.py
@@ -104,6 +104,17 @@ KIBA_STATUS_WORDS = [
     "Manifesting", "Lollygagging", "Shenaniganing", "Tomfoolering",
     "Skullduggering", "Malarkeying", "Balderdashing", "Doom-prepping",
     "Goblin-snacking", "Sweating-profusely", "Bluffing",
+    # uncensored profanity (crude swearing, non-sexual)
+    "Fuckin'-around", "Finding-out", "Clusterfucking", "Shitcanning",
+    "Bullshitting", "Jackassing", "Bastardizing", "Pissing-about",
+    "Buggering-about", "Twatwaffling", "Arsing-about", "Shit-shoveling",
+    "Crap-flinging", "Goddamn-wrangling", "Hell-raising", "Shit-stirring",
+    "Ballsing-it-up", "Cocking-it-up", "Bollocking", "Dipshittery",
+    "Assclownery", "Horse-shitting", "Batshittery", "Fuckery-manufacturing",
+    "Numbnutsing", "Dumbassing", "Half-assing", "Full-assing", "Pissant-ing",
+    "Shitstorming", "Dumpster-firing", "Tomfuckery", "Asshattery",
+    "Fucknuggeting", "Cussing-a-blue-streak", "Damn-it-all-ing",
+    "Son-of-a-bitching", "Raising-hell", "Goddamn-fiddling", "Shit-the-bedding",
 ]
 
 from src.agent import Session
